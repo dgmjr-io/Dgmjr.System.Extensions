d164f46a399242a4858d70499a9dc11e1846d62d
David G. Moore, Jr <david@dgmjr.io>
2023-10-04 17:04:29 -0400

    muchos cambios

--- src/System/IComplex.cs
+++ src/System/IComplex.cs
@@ -10,20 +10,9 @@
  *      License: MIT (https://opensource.org/licenses/MIT)
  */
 
-/*
- * IComplex.cs
- *
- *   Created: 2023-09-21-08:09:26
- *   Modified: 2023-09-21-08:09:26
- *
- *   Author: David G. Moore, Jr. <david@dgmjr.io>
- *
- *   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
- *      License: MIT (https://opensource.org/licenses/MIT)
- */
+namespace System.Numerics;
 
-namespace System;
-
+// [GenerateInterface(typeof(System.Numerics.Complex))]
 public partial interface IComplex : IComparable
 {
     double Imaginary { get; }
